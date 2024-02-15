2e92bbf9df030a00281a6fdce91c1fb75ef641c5
Waldo <92652686+LDWaldo@users.noreply.github.com>
2024-02-14 22:05:02 -0600

    Updates from sign-up to client_registration
    Changed sign_up mentions to client_registration mentions. Breaks website functionality.

--- website/auth.py
+++ website/auth.py
@@ -35,11 +35,10 @@ def logout():
     return redirect(url_for('auth.login'))
 
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/client_registration', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         username = request.form.get('username')
-        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -50,10 +49,10 @@ def sign_up():
             flash('Username must be greater than 3 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 3:
+            flash('Password must be at least 3 characters.', category='error')
         else:
-            new_user = User(username=username, first_name=first_name, password=generate_password_hash(
+            new_user = User(username=username, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
@@ -61,4 +60,4 @@ def sign_up():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
+    return render_template("client_registration.html", user=current_user)
