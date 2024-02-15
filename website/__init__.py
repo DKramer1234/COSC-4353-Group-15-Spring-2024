58e11910beaa5f76d7f34bf8161e991f614dba65
Waldo <92652686+LDWaldo@users.noreply.github.com>
2024-02-14 20:56:21 -0600

    Importing again
    Importing from the GitHub Repository instead of my own copy of the tutorial.

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,41 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don\'t care what humans think is impossible.'
-    app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix ='/')
-    app.register_blueprint(auth, url_prefix ='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
     
-    create_database(app)
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
+
 def create_database(app):
-    if not path('website/' + DB_NAME):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
