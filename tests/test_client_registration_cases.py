b27ec50cf125e201deefb3266c4ba57f3d7c24f3
Waldo <92652686+LDWaldo@users.noreply.github.com>
2024-03-29 20:52:52 -0500

    Test files finished
    Finishing up test files for test_login and test_client_registratin_cases

--- tests/test_client_registration_cases.py
+++ tests/test_client_registration_cases.py
@@ -1,15 +1,26 @@
 import pytest
 from website.models import User
 
-def test_client_registration_page(client):
-    response = client.get("/client_registration")
-    assert b"<title>Client Registration</title>" in response.data
+@pytest.mark.parametrize("username, password1, password2", [
+    ("", "", ""),
+    ("TestUsername", "", ""),
+    ("", "TestPassword", ""),
+    ("", "", "TestPassword"),
+    ("", "TestPassword", "TestPassword"),
+    ("TestUsername", "", "TestPassword"),
+    ("TestUsername", "TestPassword", ""),
+    ("TestUsername", "TestPassword", "TestPassword"),
+    ("TestUsername", "TestPassword", "!TestPassword")])
+
+def test_user_registration_cases(client, app, username, password1, password2):
+    assert client.get('/client_registration').status_code == 200
+    response = client.post('/client_registration',
+        data = {'username': username,
+                'password1': password1,
+                'password2': password2})
+    
     assert response.status_code == 200
+    data = response.data.decode('utf-8')
 
-def test_user_registration_case1(client, app):
-    response = client.post("/client_registration", data = {'username': 'TestUsername', 'password1': 'TestPassword', 'password2': 'TestPassword'})
-
-    with app.app_context():
-        assert User.query.first().username == "TestUsername"
-        assert User.query.first().check_password("TestPassword")
-        
+    if len(username) < 4:
+        assert "Username must be greater than 3 characters." in data
--- tests/test_login.py
+++ tests/test_login.py
@@ -17,3 +17,15 @@ def test_user_registration(client, app):
     with app.app_context():
         assert User.query.first().username == "TestUsername"
         assert User.query.first().check_password("TestPassword")
+
+def test_client_registration_page(client):
+    response = client.get("/client_registration")
+    assert b"<title>Client Registration</title>" in response.data
+    assert response.status_code == 200
+
+def test_user_registration_case1(client, app):
+    response = client.post("/client_registration", data = {'username': 'TestUsername', 'password1': 'TestPassword', 'password2': 'TestPassword'})
+
+    with app.app_context():
+        assert User.query.first().username == "TestUsername"
+        assert User.query.first().check_password("TestPassword")
